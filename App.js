50771086e4620fafbcae9eb8097eaa070f93bd22
Filip <Karguel1995@gmail.com>
2021-08-12 20:36:31 +0200

    deleted: console.log

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View, Pressable, Alert, Button } from 'react-native';
-import Home from './screens/Home'
-import Game from './screens/Game'
-import Rules from './screens/Rules'
-import Credits from './screens/Credits'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { StyleSheet, Text, View, Pressable, Alert, Button } from "react-native";
+import Home from "./screens/Home";
+import Game from "./screens/Game";
+import Rules from "./screens/Rules";
+import Credits from "./screens/Credits";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
 export default function App() {
-
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Game" component={Game} options={{ title: '' }}/>
+        <Stack.Screen name="Game" component={Game} options={{ title: "" }} />
         <Stack.Screen name="Rules" component={Rules} />
         <Stack.Screen name="Credits" component={Credits} />
       </Stack.Navigator>
@@ -26,60 +25,57 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F2F2F2',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F2F2F2",
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   frame: {
-    width: '80%',
-    height: '80%',
-    display: 'flex',
-    backgroundColor: '#fff',
+    width: "80%",
+    height: "80%",
+    display: "flex",
+    backgroundColor: "#fff",
     padding: 10,
-    borderRadius: 10
+    borderRadius: 10,
   },
   screen: {
-    width: '100%',
-    height: '25%',
+    width: "100%",
+    height: "25%",
   },
-  counter : {
+  counter: {
     fontSize: 12,
-    alignSelf: 'flex-end'
+    alignSelf: "flex-end",
   },
   numbers: {
-    display: 'flex',
-    height: '75%',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    alignContent: 'space-around',
-    // backgroundColor: 'black'
+    display: "flex",
+    height: "75%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    alignContent: "space-around",
   },
   keyboardNumber: {
     fontSize: 28,
-    color: 'white'
+    color: "white",
   },
   number: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     width: 70,
     height: 70,
-    backgroundColor: 'gray',
+    backgroundColor: "gray",
     borderRadius: 40,
-    color: 'white'
+    color: "white",
   },
   check: {
-    flexDirection: 'row',
+    flexDirection: "row",
     fontSize: 28,
     marginBottom: 10,
     padding: 10,
-    borderBottomColor : '#F2F2F2',
+    borderBottomColor: "#F2F2F2",
     borderBottomWidth: 4,
-
   },
   checkNumber: {
     fontSize: 28,
-  }
-
+  },
 });
--- screens/Game.js
+++ screens/Game.js
@@ -1,7 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, Pressable, Alert, Button } from 'react-native';
-import { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View, Pressable, Alert, Button } from "react-native";
+import { useState } from "react";
 
 const numbers = [
   {
@@ -42,64 +42,70 @@ const numbers = [
   },
 ];
 
+export default function Game({ navigation }) {
+  const [firstNumber, setFirstNumber] = useState("");
+  const [secondNumber, setSecondNumber] = useState("");
+  const [thirdNumber, setThirdNumber] = useState("");
+  const [previousCheck, setPreviousCheck] = useState("");
 
-export default function Game({navigation}) {
-  const [firstNumber , setFirstNumber] = useState('')
-  const [secondNumber , setSecondNumber] = useState('')
-  const [thirdNumber , setThirdNumber] = useState('')
-  const [previousCheck, setPreviousCheck] = useState('')
+  const draw = () => Math.floor(Math.random() * 9 + 1);
 
-  const draw = () => (Math.floor(Math.random() * 9 + 1))
-  
-  const [x, setX] = useState(draw())
-  const [y, setY] = useState(draw())
-  y === x ? setY(draw()) : null
-  const [z, setZ] = useState(draw())
-  z === y ? setZ(draw()) : null
-  z === x ? setZ(draw()) : null
+  const [x, setX] = useState(draw());
+  const [y, setY] = useState(draw());
+  y === x ? setY(draw()) : null;
+  const [z, setZ] = useState(draw());
+  z === y ? setZ(draw()) : null;
+  z === x ? setZ(draw()) : null;
 
-  const [counter, setCounter] = useState(0)
-  const [correctPosition, setCorrectPosition] = useState(0)
-  const [correctNumber, setCorrectNumber] = useState(0)
-
-  useEffect(() => {
-    correctPosition === 3 ? showWinAlert() : null
-  }, [correctPosition])
+  const [counter, setCounter] = useState(0);
+  const [correctPosition, setCorrectPosition] = useState(0);
+  const [correctNumber, setCorrectNumber] = useState(0);
 
   useEffect(() => {
-    thirdNumber ? checkResult() : null
-  }, [thirdNumber])
+    correctPosition === 3 ? showWinAlert() : null;
+  }, [correctPosition]);
+
+  useEffect(() => {
+    thirdNumber ? checkResult() : null;
+  }, [thirdNumber]);
 
   const checkResult = () => {
-    setCorrectPosition(0)
-    setCorrectNumber(0)
-    setCounter(prevCounter => prevCounter + 1)
+    setCorrectPosition(0);
+    setCorrectNumber(0);
+    setCounter((prevCounter) => prevCounter + 1);
 
     x == firstNumber
-    ? setCorrectPosition(prevPosition => prevPosition +1)
-      : x === secondNumber ? setCorrectNumber(prevState => prevState +1)
-        : x === thirdNumber ? setCorrectNumber(prevState => prevState +1)
-          : null
+      ? setCorrectPosition((prevPosition) => prevPosition + 1)
+      : x === secondNumber
+      ? setCorrectNumber((prevState) => prevState + 1)
+      : x === thirdNumber
+      ? setCorrectNumber((prevState) => prevState + 1)
+      : null;
     y == secondNumber
-      ? setCorrectPosition(prevPosition => prevPosition +1)
-        : y === firstNumber ? setCorrectNumber(prevState => prevState +1)
-          : y === thirdNumber ? setCorrectNumber(prevState => prevState +1)
-            : null
+      ? setCorrectPosition((prevPosition) => prevPosition + 1)
+      : y === firstNumber
+      ? setCorrectNumber((prevState) => prevState + 1)
+      : y === thirdNumber
+      ? setCorrectNumber((prevState) => prevState + 1)
+      : null;
     z == thirdNumber
-      ? setCorrectPosition(prevPosition => prevPosition +1)
-        : z === firstNumber ? setCorrectNumber(prevState => prevState +1)
-          : z === secondNumber ? setCorrectNumber(prevState => prevState +1)
-            : null
-    
-    setPreviousCheck(firstNumber.toString() + secondNumber.toString() + thirdNumber.toString())
+      ? setCorrectPosition((prevPosition) => prevPosition + 1)
+      : z === firstNumber
+      ? setCorrectNumber((prevState) => prevState + 1)
+      : z === secondNumber
+      ? setCorrectNumber((prevState) => prevState + 1)
+      : null;
 
-    setFirstNumber('')
-    setSecondNumber('')
-    setThirdNumber('')
-  }
+    setPreviousCheck(
+      firstNumber.toString() + secondNumber.toString() + thirdNumber.toString()
+    );
+
+    setFirstNumber("");
+    setSecondNumber("");
+    setThirdNumber("");
+  };
 
   const showWinAlert = () => {
-    console.log(counter)
     Alert.alert(
       `You won!
 Your attempts: ${counter}`,
@@ -116,34 +122,40 @@ Your attempts: ${counter}`,
             setX(draw());
             setY(draw());
             setZ(draw());
-            setPreviousCheck('');
-            setCorrectNumber('');
-            setCorrectPosition('');
+            setPreviousCheck("");
+            setCorrectNumber("");
+            setCorrectPosition("");
             setCounter(0);
-          }
-         },
-      ],
-    )
-  }
+          },
+        },
+      ]
+    );
+  };
 
   const displayNumbers = () => {
-    return numbers.map( (nr) => {
-      return( 
-        <Pressable 
-        key={nr.id}
-        style={styles.number}
-        onPress={() => {firstNumber ? secondNumber ? setThirdNumber(nr.number) : setSecondNumber(nr.number) : setFirstNumber(nr.number)}}
+    return numbers.map((nr) => {
+      return (
+        <Pressable
+          key={nr.id}
+          style={styles.number}
+          onPress={() => {
+            firstNumber
+              ? secondNumber
+                ? setThirdNumber(nr.number)
+                : setSecondNumber(nr.number)
+              : setFirstNumber(nr.number);
+          }}
         >
           <Text style={styles.keyboardNumber}>{nr.number}</Text>
         </Pressable>
-      )
-    })
-  } 
+      );
+    });
+  };
 
   // const randomNumberRoll = () => {
-  //   y === x ? setY(draw()) : null 
-  //   z === y ? setZ(draw()) : null 
-  //   z === x ? setZ(draw()) : null 
+  //   y === x ? setY(draw()) : null
+  //   z === y ? setZ(draw()) : null
+  //   z === x ? setZ(draw()) : null
   //   return x.toString() + y.toString() + z.toString()
   // }
 
@@ -161,9 +173,7 @@ Your attempts: ${counter}`,
           <Text>Correct number and correct position: {correctPosition}</Text>
           <Text>Correct number, but wrong position: {correctNumber}</Text>
         </View>
-        <View style={styles.numbers}>
-          {displayNumbers()}
-          </View>
+        <View style={styles.numbers}>{displayNumbers()}</View>
       </View>
       {/* <Text>Open up App.js to start working on your app!</Text>*/}
       {/* <StatusBar style="auto" /> */}
@@ -174,61 +184,59 @@ Your attempts: ${counter}`,
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F2F2F2',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F2F2F2",
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   frame: {
-    width: '80%',
-    height: '90%',
-    display: 'flex',
-    backgroundColor: '#fff',
+    width: "80%",
+    height: "90%",
+    display: "flex",
+    backgroundColor: "#fff",
     padding: 10,
-    borderRadius: 10
+    borderRadius: 10,
   },
   screen: {
-    width: '100%',
-    height: '25%',
+    width: "100%",
+    height: "25%",
   },
-  counter : {
+  counter: {
     fontSize: 12,
-    alignSelf: 'flex-end'
+    alignSelf: "flex-end",
   },
   numbers: {
-    display: 'flex',
-    height: '75%',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    alignContent: 'space-around',
+    display: "flex",
+    height: "75%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    alignContent: "space-around",
     // backgroundColor: 'black'
   },
   keyboardNumber: {
     fontSize: 28,
-    color: 'white'
+    color: "white",
   },
   number: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     width: 74,
     height: 74,
     margin: 4,
-    backgroundColor: '#595959',
+    backgroundColor: "#595959",
     borderRadius: 40,
-    color: 'white'
+    color: "white",
   },
   check: {
-    flexDirection: 'row',
+    flexDirection: "row",
     fontSize: 28,
     marginBottom: 10,
     padding: 10,
-    borderBottomColor : '#F2F2F2',
+    borderBottomColor: "#F2F2F2",
     borderBottomWidth: 4,
-
   },
   checkNumber: {
     fontSize: 28,
-  }
-
+  },
 });
